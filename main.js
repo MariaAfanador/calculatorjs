cfad4e973a445c18947ec2919ad364940e17daf0
MariaAfanador <afanador0502@gmail.com>
2024-05-23 17:42:14 -0500

    starting

---
+++ main.js
@@ -0,0 +1,18 @@
+const sumTotal = (a, b, c) => {
+  return a + b + c;
+};
+
+const subtTotal = (a, b, c) => {
+  return a - b - c;
+};
+
+const multiplyByTotal = (a, b, c) => {
+  return a * b * c;
+};
+
+const divideByTotal = (a, b, c) => {
+  return a / b / c / d / e;
+};
+
+const total = sumTotal(5, 6, 7);
+console.log(total);
